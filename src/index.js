f3b2635cc0c5d16a2cc4ff05678155972689a02a
DTKPRPT <dtkprpt@DTKPRPTs-MacBook-Pro.local>
2020-08-06 19:54:52 -0400

    Tic tac toe setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render(){
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
