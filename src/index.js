8403466282e70f969823801528b14b02367bb1af
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-07 19:52:54 -0400

    did not add texting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import { render } from '@testing-library/react';
 
 class Square extends React.Component {
     render(){
