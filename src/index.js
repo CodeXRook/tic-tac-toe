eb10bfb30874d601e9ab58bffa626bf34739cb97
DTKPRPT <dtkprpt@DTKPRPTs-MacBook-Pro.local>
2020-08-06 20:00:54 -0400

    status div set

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
+import { render } from '@testing-library/react';
 
 class Square extends React.Component {
     render(){
@@ -16,4 +17,14 @@ class Board extends React.Component {
     renderSquare(i) {
         return <Square />;
     }
+}
+
+render( {
+    const status = 'Next player: ';
+
+    return(
+        <div>
+            <div className="status">{status}</div>
+        </div>
+    )
 }
