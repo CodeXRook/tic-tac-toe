74b30f9a604fe934a25d086a45d6ff3dbe137a19
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-07 20:14:48 -0400

    display current state when clicked on replace props with state

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Square extends React.Component {
     render(){
         return (
             <button className="square" onClick={ () => alert('click')}>
-                {this.props.value}
+                {this.state.value}
             </button>
         );
     }
