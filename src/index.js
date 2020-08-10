bc39b96a20d285a0d8e380085e9394f8d6235d19
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:33:01 -0400

    upadted games history and add past moves history

--- src/index.js
+++ src/index.js
@@ -73,6 +73,15 @@ render() {
 }
 
 class Game extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
         return( 
             <div className="game">
