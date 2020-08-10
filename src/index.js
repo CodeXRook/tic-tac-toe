d5cebd07009879baee36846b5bd892a9403d9184
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:13:21 -0400

    Givrn an array of 9 squares this function will check for a winner and return declared winner

--- src/index.js
+++ src/index.js
@@ -100,6 +100,10 @@ function calculateWinner(squares) {
 
 
     for (let i = 0; i < lines.length; i++) {
-        
+        const [a,b,c] = LINES[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
     }
+    return null;
 }
