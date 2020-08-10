1104668f49f179cc5be6723d84dcc5b7a57908ea
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:40:42 -0400

    board updated

--- src/index.js
+++ src/index.js
@@ -11,17 +11,9 @@ function Square (props) {
     }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]){
+        if(calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -34,21 +26,13 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
         <Square 
-        value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}
         />
     );
 }
 
 render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-        status = 'Winner: ' + winner;
-    } else {
-        status = 'Next player: ' + (this.state.xIsNext ? ' X' : 'O');
-    }
-
     return (
         <div>
             <div className="status">{status}</div>
@@ -83,6 +67,16 @@ class Game extends React.Component  {
         };
     }
     render() {
+        const history = this.state.history;
+        const current = history[history.length -1];
+        const winner = calculateWinner(current.squares);
+        let status; 
+        if (winner){
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return( 
             <div className="game">
                 <div className="game-board">
