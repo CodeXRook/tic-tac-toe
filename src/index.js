d41dac914e4f9f1fc64331cf0dec2e9e4a371786
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:00:28 -0400

    update board handleClick function to flip the value of xIsNexr

--- src/index.js
+++ src/index.js
@@ -21,8 +21,11 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
