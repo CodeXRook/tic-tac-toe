c56af8022eb9a078d345a43b0b6b2137b40a61ad
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:03:15 -0400

    can now use both X and O and take turns, :)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square (props) {
         return (
-            <button className="square" onClick={() => this.props.onClick()}>
+            <button className="square" onClick={props.onClick}>
             {props.value}
             </button>
         );
