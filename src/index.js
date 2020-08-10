1f58ededbcbdf5248ee5924bf07fb939e3712daa
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 16:14:52 -0400

    fix board

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ function calculateWinner(squares) {
         [3,4,5],
         [6,7,8],
         [0,3,6],
-        {1,4,7},
+        [1,4,7],
         [2,5,8],
         [0,4,8],
         [2,4,6],
@@ -100,8 +100,8 @@ function calculateWinner(squares) {
 
 
     for (let i = 0; i < lines.length; i++) {
-        const [a,b,c] = LINES[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
     }
