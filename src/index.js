d9c5e57fc21ccf29e02cb0babb31c34a96f2bbd1
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 17:17:30 -0400

    updated stepNumbers

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ class Game extends React.Component  {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length -1];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
@@ -67,10 +67,11 @@ class Game extends React.Component  {
             history: history.concat([{ 
             squares: squares,
         }]),
-            xIsNext: !this.state.xIsNext,
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
         });
     }
-    .
+    
     jumpTo(step) {
         this.setState({
             stepnumber: step,
