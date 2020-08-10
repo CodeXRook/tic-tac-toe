57bcea05973c76953ba3ce8e0c38a3396a10b487
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-10 17:13:09 -0400

    added step number

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ class Game extends React.Component  {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
